5572318447e8edc751387514463768073b74b1ce
Aadesh Kulkarni <67855887+aadeshkulkarni@users.noreply.github.com>
2021-05-29 22:15:55 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -35,6 +35,7 @@ function App() {
   }, [score])
   useEffect(() => {
     if (timer > 0) {
+      console.log("UseEffect 1")
       const id = setInterval(randomGenerator, 500)
       return () => clearInterval(id)
     }
@@ -42,6 +43,7 @@ function App() {
 
   useEffect(() => {
     if (timer > 0) {
+      console.log("UseEffect 2")
       countdowntimerId = setInterval(countDown, 1000)
       return () => clearInterval(countdowntimerId)
     }
@@ -66,11 +68,11 @@ function App() {
   }
 
   return (
-    <div className="w-full h-full bg-green-500 bg-opacity-90 flex flex-col justify-center items-center relative font-raleway">
+    <div className="relative flex flex-col items-center justify-center w-full h-full bg-green-500 bg-opacity-90 font-raleway">
       {resetScreen && (
-        <div className="absolute inset-0 w-full h-hull bg-gray-800 flex flex-col justify-center items-center z-10">
-          <div className="absolute inset-0 w-full h-hull bg-black flex flex-col justify-center items-center animate-pulse z-10">
-            <h1 className="text-white text-xl animate-ping">Get Ready</h1>
+        <div className="absolute inset-0 z-10 flex flex-col items-center justify-center w-full bg-gray-800 h-hull">
+          <div className="absolute inset-0 z-10 flex flex-col items-center justify-center w-full bg-black h-hull animate-pulse">
+            <h1 className="text-xl text-white animate-ping">Get Ready</h1>
           </div>
         </div>
       )}
@@ -93,19 +95,18 @@ function App() {
         }}
       />
       {localStorage.getItem("highScore") && (
-        <h1 className="text-2xl text-gray-800  p-2">
+        <h1 className="p-2 text-2xl text-gray-800">
           Highest score: {localStorage.getItem("highScore")}
         </h1>
       )}
-      <h1 className="text-2xl text-gray-800  p-2">Score: {score}</h1>
-      <h1 className="text-3xl text-gray-800  p-2 uppercase"> Whack A Mole</h1>
-      <h2 className="text-2xl text-gray-800 p-2">Time Left: {timer}</h2>
-      <div className="flex flex-wrap bg-green-700 w-320 h-320 shadow-xl rounded-sm p-1">
+      <h1 className="p-2 text-2xl text-gray-800">Score: {score}</h1>
+      <h1 className="p-2 text-3xl text-gray-800 uppercase"> Whack A Mole</h1>
+      <h2 className="p-2 text-2xl text-gray-800">Time Left: {timer}</h2>
+      <div className="flex flex-wrap p-1 bg-green-700 rounded-sm shadow-xl w-320 h-320">
         <div
           className="bg-green-800 bg-opacity-90 h-100 w-100 m-0.5 shadow-lg hover:bg-green-900 cursor-pointer active:bg-green-700 flex justify-center items-center animate-pulse"
           onClick={() => {
             timer > 0 && moleNumber === 1 && setScore(score + 50)
-            setMoleNumber()
           }}
         >
           {moleNumber === 1 && <img src={mole} className="w-20 h-20" />}
@@ -114,7 +115,6 @@ function App() {
           className="bg-green-800 bg-opacity-90 h-100 w-100 m-0.5 shadow-lg hover:bg-green-900 cursor-pointer active:bg-green-700 flex justify-center items-center animate-pulse"
           onClick={() => {
             timer > 0 && moleNumber === 2 && setScore(score + 50)
-            setMoleNumber()
           }}
         >
           {moleNumber === 2 && <img src={mole} className="w-20 h-20" />}
@@ -123,7 +123,6 @@ function App() {
           className="bg-green-800 bg-opacity-90 h-100 w-100 m-0.5 shadow-lg hover:bg-green-900 cursor-pointer active:bg-green-700 flex justify-center items-center animate-pulse"
           onClick={() => {
             timer > 0 && moleNumber === 3 && setScore(score + 50)
-            setMoleNumber()
           }}
         >
           {moleNumber === 3 && <img src={mole} className="w-20 h-20" />}
@@ -132,7 +131,6 @@ function App() {
           className="bg-green-800 bg-opacity-90 h-100 w-100 m-0.5 shadow-lg hover:bg-green-900 cursor-pointer active:bg-green-700 flex justify-center items-center animate-pulse"
           onClick={() => {
             timer > 0 && moleNumber === 4 && setScore(score + 50)
-            setMoleNumber()
           }}
         >
           {moleNumber === 4 && <img src={mole} className="w-20 h-20" />}
@@ -141,7 +139,6 @@ function App() {
           className="bg-green-800 bg-opacity-90 h-100 w-100 m-0.5 shadow-lg hover:bg-green-900 cursor-pointer active:bg-green-700 flex justify-center items-center animate-pulse"
           onClick={() => {
             timer > 0 && moleNumber === 5 && setScore(score + 50)
-            setMoleNumber()
           }}
         >
           {moleNumber === 5 && <img src={mole} className="w-20 h-20" />}
@@ -150,7 +147,6 @@ function App() {
           className="bg-green-800 bg-opacity-90 h-100 w-100 m-0.5 shadow-lg hover:bg-green-900 cursor-pointer active:bg-green-700 flex justify-center items-center animate-pulse"
           onClick={() => {
             timer > 0 && moleNumber === 6 && setScore(score + 50)
-            setMoleNumber()
           }}
         >
           {moleNumber === 6 && <img src={mole} className="w-20 h-20" />}
@@ -159,7 +155,6 @@ function App() {
           className="bg-green-800 bg-opacity-90 h-100 w-100 m-0.5 shadow-lg hover:bg-green-900 cursor-pointer active:bg-green-700 flex justify-center items-center animate-pulse"
           onClick={() => {
             timer > 0 && moleNumber === 7 && setScore(score + 50)
-            setMoleNumber()
           }}
         >
           {moleNumber === 7 && <img src={mole} className="w-20 h-20" />}
@@ -168,7 +163,6 @@ function App() {
           className="bg-green-800 bg-opacity-90 h-100 w-100 m-0.5 shadow-lg hover:bg-green-900 cursor-pointer active:bg-green-700 flex justify-center items-center animate-pulse"
           onClick={() => {
             timer > 0 && moleNumber === 8 && setScore(score + 50)
-            setMoleNumber()
           }}
         >
           {moleNumber === 8 && <img src={mole} className="w-20 h-20" />}
@@ -177,14 +171,13 @@ function App() {
           className="bg-green-800 bg-opacity-90 h-100 w-100 m-0.5 shadow-lg hover:bg-green-900 cursor-pointer active:bg-green-700 flex justify-center items-center animate-pulse"
           onClick={() => {
             timer > 0 && moleNumber === 9 && setScore(score + 50)
-            setMoleNumber()
           }}
         >
           {moleNumber === 9 && <img src={mole} className="w-20 h-20" />}
         </div>
       </div>
       <h6
-        className="absolute bottom-2 font-semibold hover:opacity-50"
+        className="absolute font-semibold bottom-2 hover:opacity-50"
         onClick={(e) => {
           e.preventDefault()
           window.location.href = "https://aadeshkulkarni.me/"
@@ -194,16 +187,16 @@ function App() {
         Kulkarni
       </h6>
       {showResult && (
-        <div className="absolute inset-0 w-full h-hull bg-black flex flex-col justify-center items-center">
-          <h2 className="text-3xl text-white  p-2">Whack A Mole</h2>
+        <div className="absolute inset-0 flex flex-col items-center justify-center w-full bg-black h-hull">
+          <h2 className="p-2 text-3xl text-white">Whack A Mole</h2>
           <img src={mole} className="w-24 h-24 m-6" />
           {localStorage.getItem("highScore") &&
             localStorage.getItem("highScore") !== "" && (
-              <h2 className="text-2xl text-white  p-2">
+              <h2 className="p-2 text-2xl text-white">
                 Highest score: {localStorage.getItem("highScore")}
               </h2>
             )}
-          <h2 className="text-2xl text-white  p-2">Current score: {score}</h2>
+          <h2 className="p-2 text-2xl text-white">Current score: {score}</h2>
           <button
             onClick={() => {
               setTimer(COUNTDOWN_TIMER)
@@ -211,18 +204,18 @@ function App() {
               randomGenerator()
               setShowResult(false)
             }}
-            className="bg-white p-4 w-8/12 text-xl m-2 rounded-lg shadow-lg border border-gray-800 focus:outline-none active:outline-none active:bg-gray-50 md:w-4/12"
+            className="w-8/12 p-4 m-2 text-xl bg-white border border-gray-800 rounded-lg shadow-lg focus:outline-none active:outline-none active:bg-gray-50 md:w-4/12"
           >
             Play Again
           </button>
         </div>
       )}
       {initial && (
-        <div className="absolute inset-0 w-full h-hull bg-black flex flex-col justify-center items-center">
-          <h2 className="text-3xl text-white  p-2">Whack A Mole</h2>
+        <div className="absolute inset-0 flex flex-col items-center justify-center w-full bg-black h-hull">
+          <h2 className="p-2 text-3xl text-white">Whack A Mole</h2>
           <img src={mole} className="w-24 h-24 m-6" />
-          <h3 className="text-xl text-white  p-2">Rules:</h3>
-          <p className="text-base text-white p-3 w-7/12 text-center">
+          <h3 className="p-2 text-xl text-white">Rules:</h3>
+          <p className="w-7/12 p-3 text-base text-center text-white">
             Well, there are no rules! <br />
             Just Whack the mole <br />
             Whack it real bad!
@@ -235,7 +228,7 @@ function App() {
               setScore(0)
               randomGenerator()
             }}
-            className="bg-white p-4 w-8/12 text-xl m-2 rounded-lg shadow-lg border border-gray-800 focus:outline-none active:outline-none active:bg-gray-50 md:w-4/12"
+            className="w-8/12 p-4 m-2 text-xl bg-white border border-gray-800 rounded-lg shadow-lg focus:outline-none active:outline-none active:bg-gray-50 md:w-4/12"
           >
             Play
           </button>
