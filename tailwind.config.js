37dc48cd0c834d556bf029d1ec492d2c39094aaf
Aadesh Kulkarni <67855887+aadeshkulkarni@users.noreply.github.com>
2021-06-01 02:50:10 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
+const colors = require("tailwindcss/colors")
+
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
+      colors: {
+        emerald: colors.emerald,
+        teal: colors.teal,
+      },
       height: {
         320: "340px",
         100: "100px",
@@ -24,10 +30,16 @@ module.exports = {
           "0%, 100%": { transform: "rotate(-3deg)" },
           "50%": { transform: "rotate(3deg)" },
         },
+        background: {
+          "0%": { backgroundColor: "#047857" },
+          "50%": { backgroundColor: "#0E7490" },
+          "100%": { backgroundColor: "#14B8A6" },
+        },
       },
       animation: {
         show: "show 1s ease-in-out",
         wiggle: "wiggle 1s ease-in-out infinite",
+        background: "background 3s ease-in-out",
       },
     },
   },
