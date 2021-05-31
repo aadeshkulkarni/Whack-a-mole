b19c6df31f66380acb19f1a0532bab99f1b43ccc
Aadesh Kulkarni <67855887+aadeshkulkarni@users.noreply.github.com>
2021-06-01 01:45:41 +0530

    Create config.js

---
+++ src/config.js
@@ -0,0 +1,9 @@
+export const config = {
+  apiKey: "AIzaSyCBtKXm-wJKAhTzYALwAD2DN5bsRFX6MBc",
+  authDomain: "whackamole-979dc.firebaseapp.com",
+  projectId: "whackamole-979dc",
+  storageBucket: "whackamole-979dc.appspot.com",
+  messagingSenderId: "639561485134",
+  appId: "1:639561485134:web:796356e8bc94bb0e57ac1f",
+  measurementId: "G-E4M7TB1CN0",
+}
