c32a39558c3a02e7a86056a6649160b6b84a1b34
Aadesh Kulkarni <67855887+aadeshkulkarni@users.noreply.github.com>
2021-06-01 05:30:58 +0530

    Update firestore.js

--- src/services/firestore.js
+++ src/services/firestore.js
@@ -23,18 +23,47 @@ export const addLeaderBoard = (name, score) => {
   })
 }
 
+// export const getLeaderboard = async () => {
+//   try {
+//     let leaderboard = []
+//     var boardRef = db.collection("leaderBoard")
+//     let allleadersSnapShot = await boardRef.get()
+//     allleadersSnapShot.forEach((doc) => {
+//       let currentDoc = doc.data()
+//       leaderboard.push({ user: currentDoc.user, score: currentDoc.score })
+//     })
+//     leaderboard = leaderboard.sort((a, b) => b.score - a.score)
+//     const top5 = leaderboard.splice(0, 5)
+
+//     return top5
+//   } catch (err) {
+//     console.log("Error getting documents", err)
+//   }
+// }
+
 export const getLeaderboard = async () => {
   try {
     let leaderboard = []
     var boardRef = db.collection("leaderBoard")
-    let allleadersSnapShot = await boardRef.get()
+
+    let allleadersSnapShot = await boardRef.orderBy("score", "desc").get()
+    // let allleadersSnapShot = await boardRef.get()
     allleadersSnapShot.forEach((doc) => {
       let currentDoc = doc.data()
       leaderboard.push({ user: currentDoc.user, score: currentDoc.score })
     })
-    leaderboard = leaderboard.sort((a, b) => b.score - a.score)
-    const top5 = leaderboard.splice(0, 5)
 
+    const users = []
+    const top5 = []
+    for (let i = 0; i < leaderboard.length; i++) {
+      if (!users.includes(leaderboard[i].user)) {
+        users.push(leaderboard[i].user)
+        top5.push({ user: leaderboard[i].user, score: leaderboard[i].score })
+      }
+      if (users.length === 5) {
+        break
+      }
+    }
     return top5
   } catch (err) {
     console.log("Error getting documents", err)
