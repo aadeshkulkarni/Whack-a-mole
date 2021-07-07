f7ffeeea785d83255ca21f0a8367ac0f5365b910
Aadesh Kulkarni <67855887+aadeshkulkarni@users.noreply.github.com>
2021-07-07 13:52:07 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -160,7 +160,7 @@ function App() {
       <div className="absolute flex items-center justify-around w-8/12 top-5 lg:top-20">
         <h1 className="p-2 text-3xl text-gray-800"> Whack-A-Mole</h1>
       </div>
-      <div className="absolute top-15" id="websdk">
+      <div className="absolute z-40 w-56 h-56 top-15" id="websdk">
 
       </div>
       <h1 className="p-2 text-2xl font-light text-gray-800">{name}</h1>
